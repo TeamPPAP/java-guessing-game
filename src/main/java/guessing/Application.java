5204ab63f69269d8661038f3e4f443e1c6f16e2c
kimsunwook <kckk116560@gmail.com>
2025-09-24 19:00:38 +0900

    second commit

--- src/main/java/guessing/Application.java
+++ src/main/java/guessing/Application.java
@@ -1,7 +1,30 @@
 package guessing;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 실행 코드 작성
+
+        int randomNum= (int) (Math.random() * (100 - 1) + 1);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try{
+                System.out.print("플레이어의 수를 입력해주세요: ");
+                String playerNum = scanner.nextLine();
+
+                if (playerNum == null || playerNum.isBlank()) {
+                    throw new IllegalArgumentException("잘못된 입력을 하셨습니다. 다시 입력해주세요.");
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+
+        }
+
+
     }
 }
